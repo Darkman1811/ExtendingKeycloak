fa607b2d67fb9ad81b1d0e2b29ab8876b46f64c4
Abdoulaye Diaw <abdoulaye.diaw@joza-it.fr>
2025-01-09 23:20:30 +0100

    Overrinding spi

--- src/main/java/com/gwidgets/VoidEmailSenderProvider.java
+++ src/main/java/com/gwidgets/VoidEmailSenderProvider.java
@@ -1,2 +1,24 @@
-package com.gwidgets;public class VoidEmailSenderProvider {
+package com.gwidgets;
+
+import org.keycloak.email.EmailException;
+import org.keycloak.email.EmailSenderProvider;
+import org.keycloak.models.UserModel;
+
+import java.util.Map;
+
+public class VoidEmailSenderProvider implements EmailSenderProvider {
+    @Override
+    public void send(Map<String, String> config, UserModel user, String subject, String textBody, String htmlBody) throws EmailException {
+        System.out.println("Sending email to "+user.getEmail()+" with subject "+subject);
+    }
+
+    @Override
+    public void send(Map<String, String> map, String s, String s1, String s2, String s3) throws EmailException {
+        System.out.println("Sending email to "+s+" with subject "+s1+" and body "+s2);
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
--- src/main/java/com/gwidgets/VoidEmailSenderProviderFactory.java
+++ src/main/java/com/gwidgets/VoidEmailSenderProviderFactory.java
@@ -1,2 +1,47 @@
-package com.gwidgets;public class VoidEmailSenderProviderFactory {
+package com.gwidgets;
+
+import org.keycloak.Config;
+import org.keycloak.email.EmailSenderProvider;
+import org.keycloak.email.EmailSenderProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+public class VoidEmailSenderProviderFactory implements EmailSenderProviderFactory {
+    @Override
+    public EmailSenderProvider create(KeycloakSession keycloakSession) {
+        return new VoidEmailSenderProvider();
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "default";
+    }
+
+    @Override
+    public int order() {
+        return 1;
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigMetadata() {
+        return EmailSenderProviderFactory.super.getConfigMetadata();
+    }
 }

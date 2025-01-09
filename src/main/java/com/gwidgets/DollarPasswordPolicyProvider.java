4c61ba8bd528b13bac85d269efc5a3ac39e2508b
Abdoulaye Diaw <abdoulaye.diaw@joza-it.fr>
2025-01-09 23:20:20 +0100

    extending spi

--- src/main/java/com/gwidgets/DollarPasswordPolicyProvider.java
+++ src/main/java/com/gwidgets/DollarPasswordPolicyProvider.java
@@ -3,9 +3,37 @@ package com.gwidgets;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
 import org.keycloak.policy.PasswordPolicyProvider;
 import org.keycloak.policy.PasswordPolicyProviderFactory;
+import org.keycloak.policy.PolicyError;
 
-public class DollarPasswordPolicyProvider   {
+public class DollarPasswordPolicyProvider implements PasswordPolicyProvider   {
 
+    @Override
+    public PolicyError validate(RealmModel realmModel, UserModel userModel, String s) {
+        return validate(null,s);
+    }
+
+    @Override
+    public PolicyError validate(String s, String s1) {
+
+        if(s1!=null) {
+            return s1.startsWith("$") ? null : new PolicyError("Password must start with $");
+        }else {
+            System.out.println("Password is null");
+            return null;
+        }
+    }
+
+    @Override
+    public Object parseConfig(String s) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
--- src/main/java/com/gwidgets/DollarPasswordPolicyProviderFactory.java
+++ src/main/java/com/gwidgets/DollarPasswordPolicyProviderFactory.java
@@ -1,2 +1,54 @@
-package com.gwidgets;public class DollarPasswordPolicyProviderFactory {
+package com.gwidgets;
+
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.policy.PasswordPolicyProvider;
+import org.keycloak.policy.PasswordPolicyProviderFactory;
+
+public class DollarPasswordPolicyProviderFactory implements PasswordPolicyProviderFactory {
+    @Override
+    public String getDisplayName() {
+        return "Dollar Policy Password";
+    }
+
+    @Override
+    public String getConfigType() {
+        return null;
+    }
+
+    @Override
+    public String getDefaultConfigValue() {
+        return null;
+    }
+
+    @Override
+    public boolean isMultiplSupported() {
+        return false;
+    }
+
+    @Override
+    public PasswordPolicyProvider create(KeycloakSession keycloakSession) {
+        return new DollarPasswordPolicyProvider();
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "dollarPasswordPolicy";
+    }
 }
